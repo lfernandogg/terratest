17f6becbfc457b0ee7ce48cc845d80bd36fbcbef
Luis Fernando Gutierrez Guerrero <lfernandogg@gmail.com>
2022-03-23 13:57:16 -0600

    test

--- firstServer/instance.tf
+++ firstServer/instance.tf
@@ -1,21 +1,25 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~> 3.0"
+        }
     }
-  }
-}
-provider "aws" {
-  access_key = "XXXXXXXXXXXXXXXX"
-  secret_key = "XXXXXXXXXXXXXXXXXX"
-  region = "us-east-2"
 }
 
-resource "aws_instance" "hello-instance" {
-  ami = "ami-0fb653ca2d3203ac1"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "hello-instance"
-  }
+variable "AWS_ACCESS_KEY_ID" {}
+variable "AWS_SECRET_KEY_ID" {}
+
+provider "aws" {
+  region     = "us-east-2"
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_KEY_ID
+}
+
+resource "aws_instance" "instance-terraformcloud" {
+    ami = "ami-0fb653ca2d3203ac1"
+    instance_type = "t2.micro"
+    tags = {
+        Name = "terraformcloud"
+    }
 }
